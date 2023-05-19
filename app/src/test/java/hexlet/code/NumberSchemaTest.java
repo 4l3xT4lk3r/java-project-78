4ac50b91e6ca47f3d8d2c49a6ce4b7ac5e6f4ba1
Alexey Besedin <cdr800mb@mail.ru>
2023-05-20 00:15:08 +0400

    Fix NumberSchemaTests

--- app/src/test/java/hexlet/code/NumberSchemaTest.java
+++ app/src/test/java/hexlet/code/NumberSchemaTest.java
@@ -8,6 +8,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NumberSchemaTest {
 
+    public static final int GOOD_NUMBER_1 = 10;
+    public static final int BAD_NUMBER_1 = -10;
+    public static final int BAD_NUMBER_2 = 0;
+    public static final int BAD_NUMBER_3 = 4;
+    public static final int BAD_NUMBER_4 = 11;
+
+    public static final int MIN_NUMBER_RANGE = 5;
+    public static final int MAX_NUMBER_RANGE = 10;
+
     @Test
     public void testNumberSchema() {
         Validator validator = new Validator();
@@ -17,13 +26,13 @@ public class NumberSchemaTest {
         schema.required();
         assertFalse(schema.isValid(null));
         assertFalse(schema.isValid("5"));
-        assertTrue(schema.isValid(10));
-        assertFalse(schema.isValid(-10));
-        assertFalse(schema.isValid(0));
-        schema.range(5, 10);
-        assertTrue(schema.isValid(5));
-        assertTrue(schema.isValid(10));
-        assertFalse(schema.isValid(4));
-        assertFalse(schema.isValid(11));
+        assertTrue(schema.isValid(GOOD_NUMBER_1));
+        assertFalse(schema.isValid(BAD_NUMBER_1));
+        assertFalse(schema.isValid(BAD_NUMBER_2));
+        schema.range(MIN_NUMBER_RANGE, MAX_NUMBER_RANGE);
+        assertTrue(schema.isValid(MIN_NUMBER_RANGE));
+        assertTrue(schema.isValid(MAX_NUMBER_RANGE));
+        assertFalse(schema.isValid(BAD_NUMBER_3));
+        assertFalse(schema.isValid(BAD_NUMBER_4));
     }
 }
