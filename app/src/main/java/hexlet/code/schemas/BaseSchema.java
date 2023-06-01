6a3836171c3883e723d8907207b63b89c6569fbc
Alexey Besedin <cdr800mb@mail.ru>
2023-06-02 00:39:02 +0400

    Remove getter for field due to broken incapsulation

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -7,16 +7,12 @@ import java.util.function.Predicate;
 public abstract class BaseSchema {
     private final List<Predicate<Object>> checks = new ArrayList<>();
 
-    public final List<Predicate<Object>> getChecks() {
-        return checks;
-    }
-
     public final void addCheck(Predicate<Object> predicate) {
         checks.add(predicate);
     }
 
     public final boolean isValid(Object data) {
-        return getChecks().stream().allMatch(p -> p.test(data));
+        return checks.stream().allMatch(p -> p.test(data));
     }
 
 }
